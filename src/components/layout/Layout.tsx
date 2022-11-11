3f1df4309a5541bf1f00c482bf607aec049b9eff
승욱 김 <sw.kim@kyobobook.com>
2022-11-11 22:23:26 +0900

    layout 보완

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,13 @@
+interface ILayoutProps {
+  children: JSX.Element
+}
+
+export default function Layout({ children }: ILayoutProps) {
+  return (
+    <div className="content">
+      <div className="side" />
+      <div className="main">{children}</div>
+      <div className="side" />
+    </div>
+  )
+}
