8dfdebc886fa054c9cbb40de4edc586df0c11c23
승욱 김 <sw.kim@kyobobook.com>
2022-11-30 21:13:09 +0900

    commit

--- out/_next/static/pGSwSaRm8yrY3lL1EZGSg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/355-4ba038baa407d161.js","static/chunks/pages/index-057e7222f48b722c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
