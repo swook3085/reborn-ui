387c9dffc324deee5e82318384968d7d98da5ea0
승욱 김 <sw.kim@kyobobook.com>
2022-11-30 21:23:54 +0900

    test

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,1 +1,4 @@
-export const LOCAL_DOMAIN = "http://localhost:8081";
+export const LOCAL_DOMAIN = 'http://localhost:8081'
+export const MIN_Y = 60 // 바텀시트가 최대로 높이 올라갔을 때의 y 값
+export const MAX_Y = window.innerHeight - 80 // 바텀시트가 최소로 내려갔을 때의 y 값
+export const BOTTOM_SHEET_HEIGHT = window.innerHeight - MIN_Y // 바텀시트의 세로 길이
