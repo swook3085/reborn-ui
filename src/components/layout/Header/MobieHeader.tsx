8e75f6a07099937cd8a4d45b678754d79e6e911a
승욱 김 <sw.kim@kyobobook.com>
2022-12-05 22:43:24 +0900

    mobileHeader onOpen 추가

--- src/components/layout/Header/MobieHeader.tsx
+++ src/components/layout/Header/MobieHeader.tsx
@@ -1,13 +1,17 @@
+import { onOpen } from '@modules/store/slices/bottomModal'
 import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined'
+import { useDispatch } from 'react-redux'
 
 const MobileHeader = () => {
+  const dispatch = useDispatch()
+
   return (
     <div className='header'>
       <div className='logo-wrap'>
         <div className='logo'></div>
       </div>
       <div className='filter-wrap'>
-        <button>
+        <button onClick={() => dispatch(onOpen())}>
           <FilterAltOutlinedIcon fontSize='large' />
         </button>
       </div>
