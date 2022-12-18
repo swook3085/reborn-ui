a923036e6b14b9160931249ae28651840019ad74
승욱 김 <sw.kim@kyobobook.com>
2022-12-18 14:51:15 +0900

    배포 오류 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,83 +35,83 @@ export default function Home() {
   const [endDate, setEndDate] = useState<string>(dateFormatDash(new Date()))
 
   useEffect(() => {
-    getSidoList()
-    getKindList(upKind)
+    // getSidoList()
+    // getKindList(upKind)
   }, [])
 
-  const getSigunguList = async (uprCd: string) => {
-    const params = {
-      uprCd,
-    }
-    setSido(uprCd)
-    const data = await selectSigunguList(params)
-    console.log(data)
-    setSigunguList(data)
-  }
+  // const getSigunguList = async (uprCd: string) => {
+  //   const params = {
+  //     uprCd,
+  //   }
+  //   setSido(uprCd)
+  //   const data = await selectSigunguList(params)
+  //   console.log(data)
+  //   setSigunguList(data)
+  // }
 
-  const getSidoList = async () => {
-    const data = await selectSidoList({ numOfRows: '20' })
-    console.log(data)
-    // try {
-    //   const list = response.data.response.body.items.item || []
-    //   res.status(200).json(list)
-    // } catch (error) {
-    //   res.status(500).json([])
-    // }
+  // const getSidoList = async () => {
+  //   const data = await selectSidoList({ numOfRows: '20' })
+  //   console.log(data)
+  //   // try {
+  //   //   const list = response.data.response.body.items.item || []
+  //   //   res.status(200).json(list)
+  //   // } catch (error) {
+  //   //   res.status(500).json([])
+  //   // }
 
-    setSidoList(data)
-    getSigunguList(data[0].orgCd)
-  }
+  //   setSidoList(data)
+  //   getSigunguList(data[0].orgCd)
+  // }
 
-  const getKindList = async (upKindCd: string) => {
-    const params = {
-      upKindCd,
-    }
-    setLoading(true)
-    setUpKind(upKindCd)
-    const data = await selectKindList(params)
-    console.log(data)
-    const kindSelectData = data.map(({ knm, kindCd }) => {
-      return {
-        value: kindCd,
-        label: knm,
-      }
-    })
-    setKindList([{ value: '', label: '전체' }, ...kindSelectData])
-    setKind(data[0].kindCd)
-    setLoading(false)
-  }
+  // const getKindList = async (upKindCd: string) => {
+  //   const params = {
+  //     upKindCd,
+  //   }
+  //   setLoading(true)
+  //   setUpKind(upKindCd)
+  //   const data = await selectKindList(params)
+  //   console.log(data)
+  //   const kindSelectData = data.map(({ knm, kindCd }) => {
+  //     return {
+  //       value: kindCd,
+  //       label: knm,
+  //     }
+  //   })
+  //   setKindList([{ value: '', label: '전체' }, ...kindSelectData])
+  //   setKind(data[0].kindCd)
+  //   setLoading(false)
+  // }
 
-  const getPetList = async () => {
-    const params: IPetParams = {
-      bgnde: dateToString(dayjs(startDate, 'YYYYMMDD').toDate()),
-      endde: endDate,
-      upKind,
-      kind,
-      uprCd: sido,
-      orgCd: sigungu,
-      page: '1',
-      limit: '20',
-      state: 'notice',
-    }
-    console.log('params', params)
-    const list = await selectPetList(params)
-    console.log(list)
-  }
+  // const getPetList = async () => {
+  //   const params: IPetParams = {
+  //     bgnde: dateToString(dayjs(startDate, 'YYYYMMDD').toDate()),
+  //     endde: endDate,
+  //     upKind,
+  //     kind,
+  //     uprCd: sido,
+  //     orgCd: sigungu,
+  //     page: '1',
+  //     limit: '20',
+  //     state: 'notice',
+  //   }
+  //   console.log('params', params)
+  //   const list = await selectPetList(params)
+  //   console.log(list)
+  // }
 
   const onClick = () => {
     console.log('조회')
-    getPetList()
+    // getPetList()
   }
 
-  const onChange: DatePickerProps['onChange'] = (date, dateString) => {
-    console.log(date, dateString)
-    setStartDate(dateString)
-  }
+  // const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+  //   console.log(date, dateString)
+  //   setStartDate(dateString)
+  // }
 
   return (
     <>
-      <Button type='primary'></Button>
+      {/* <Button type='primary'></Button>
       <button onClick={() => setOpen(true)}>열기</button>
       <Select
         labelInValue
@@ -146,8 +146,8 @@ export default function Home() {
             </option>
           )
         })}
-      </select>
-      <CusDatePicker
+      </select> */}
+      {/* <CusDatePicker
         value={dayjs(startDate, 'YYYY-MM-DD')}
         disabledDate={(d) => !d || d.isAfter(endDate)}
         onChange={onChange}
@@ -156,7 +156,7 @@ export default function Home() {
         value={dayjs(endDate, 'YYYY-MM-DD')}
         disabledDate={(d) => !d || d.isAfter(endDate) || d.isBefore(startDate)}
         onChange={onChange}
-      />
+      /> */}
       {/* <CusDatePicker
         value={startDate}
         max={endDate}
